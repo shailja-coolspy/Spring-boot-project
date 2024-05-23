9978f929a6330a3d731e6fd1adeba6fe5e83f10f
shailja-coolspy <agarwalshailja1@gmail.com>
2024-05-23 12:37:46 +0530

    initial commit

---
+++ bookRealm_api_v1/src/main/java/com/bookRealm/api_v1/ApiV1Application.java
@@ -0,0 +1,13 @@
+package com.bookRealm.api_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiV1Application.class, args);
+	}
+
+}
---
+++ bookRealm_api_v1/src/test/java/com/bookRealm/api_v1/ApiV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookRealm.api_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
